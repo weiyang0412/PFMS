dbb09614983e905df455623843daf74e2b3e61e8
weiyang0412 <weiyang.wyc@gmail.com>
2026-07-07 23:44:12 +0800

    router index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -21,12 +21,12 @@ import { useUserStore } from '../stores/userStore'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        // {
-        //     path: '/',
-        //     name: 'home',
-        //     component: HomeView,
-        //     meta: { requiresAuth: true },
-        // },
+        {
+            path: '/',
+            name: "dashboard",
+            component: Dashboard,
+            meta: { requiresAuth: true },
+        },
         {
             path: "/register",
             name: "register",
