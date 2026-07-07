29f6efd0727eb08bb4d4a0665a3db2a8e80f1da3
weiyang0412 <weiyang.wyc@gmail.com>
2026-07-07 20:06:32 +0800

    domain setting

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const apiBaseUrl = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
     baseURL: apiBaseUrl,
